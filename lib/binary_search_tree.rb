ab0af8f3590bf38cacf44c7eb068b0dfed76fe39
Axton Smith <axtonsmith@gmail.com>
2022-08-08 08:52:14 -0400

    Build Node and Tree classes

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,3 +1,17 @@
 # frozen_string_literal: true
 
+class Node
+  require 'Comparable'
+  attr_accessor :data, :left_child, :right_child
+end
 
+class Tree
+  attr_accessor :root
+
+  def initialize(array)
+    @root = build_tree(array)
+  end
+  def build_tree(array)
+    
+  end
+end
