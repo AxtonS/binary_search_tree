1bf1a36b4a1a2a9d0595da37df7e397f21dc8e3f
Axton Smith <axtonsmith@gmail.com>
2022-08-09 10:14:58 -0400

    Finish insert method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 # builds node objects to populate binary search tree
 class Node
   attr_accessor :data, :left_child, :right_child
@@ -28,8 +30,21 @@ class Tree
     mid = (head + tail) / 2
     Node.new(array[mid], build_tree(array, head, mid - 1), build_tree(array, mid + 1, tail))
   end
+
+  def insert(value, node = @root)
+    if node.nil?
+      Node.new(value)
+    elsif value < node.data
+      node.left_child = insert(value, node.left_child)
+      node
+    elsif value > node.data
+      node.right_child = insert(value, node.right_child)
+      node
+    end
+  end
 end
 
 array = [1,7,7,3,11,1,2,3,4,5,6,6,6]
 tree = Tree.new(array)
-p tree.root.right_child.right_child.right_child.right_child.data
+tree.insert(400)
+p tree.root.data
